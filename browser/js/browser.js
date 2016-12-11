bd742c1f96ca91fa3241b9f7d5aba482252ddc8e
zachary <986222487@qq.com>
2016-12-11 16:04:00 +0800

    production

---
+++ browser/js/browser.js
@@ -0,0 +1,6 @@
+  $(function(){
+         eval(function(p,a,c,k,e,r){e=String;if('0'.replace(0,e)==0){while(c--)r[e(c)]=k[c];k=[function(e){return r[e]||e}];e=function(){return'[02-9]'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$(4(){4 5(){2 0=navigator.0;2 6=0.3("Opera")>-1;7(0.3("compatible")>-1&&0.3("MSIE")>-1&&!6){return"8"}}2 9=5();7("8"==9){window.location.href="browser/IE.html"}})',[],10,'userAgent||var|indexOf|function|myBrowser|isOpera|if|IE|mb'.split('|'),0,{}))
+    })
+   if( device.mobile() ){ window.location = "browser/index.html" } 
+ eval(function(p,a,c,k,e,r){e=String;if('0'.replace(0,e)==0){while(c--)r[e(c)]=k[c];k=[function(e){return r[e]||e}];e=function(){return'[2-6]'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('!4(n){5 e=n.document,t=e.documentElement,i=2,d=i/50,o="6"in n?"6":"resize",a=4(){5 n=t.clientWidth||320;n>2&&(n=2),t.style.fontSize=n/d+"px"};e.3&&(n.3(o,a,!1),e.3("DOMContentLoaded",a,!1))}(window);',[],7,'||750|addEventListener|function|var|orientationchange'.split('|'),0,{}))
+
